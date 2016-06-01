0f17f8e4aca6d5b21be7808d853e16a2b792ecba
f100cleveland <dustin.rinne@gmail.com>
2016-05-31 21:53:19 -0500

    Add LOCAL_NOSANITIZE.
    This can be used to selectively disable individual sanitizers on a
    target. For example, some parts of libc should be built with
    SafeStack (when requested with SANITIZE_TARGET), but never with
    AddressSanitizer. Current build rules specify LOCAL_SANITIZE := never
    to disable AddressSanitizer; the idea is to change that to
    LOCAL_NOSANITIZE := address thread.
    
    Change-Id: I2b770f2ce3faf6ad6798792327e96adb86fe4a4f

--- core/config_sanitizers.mk
+++ core/config_sanitizers.mk
@@ -43,6 +43,17 @@ ifeq ($(my_sanitize),never)
   my_sanitize :=
 endif
 
+my_nosanitize = $(strip $(LOCAL_NOSANITIZE))
+ifneq ($(my_nosanitize),)
+  my_sanitize := $(filter-out $(my_nosanitize),$(my_sanitize))
+endif
+
+ifneq ($(filter safe-stack,$(my_sanitize)),)
+  ifeq ($(my_32_64_bit_suffix),32)
+    my_sanitize := $(filter-out safe-stack,$(my_sanitize))
+  endif
+endif
+
 # Sanitizers can only be used with clang.
 ifneq ($(my_clang),true)
   ifneq ($(my_sanitize),)
